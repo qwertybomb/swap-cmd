5ad8a69aa48abe4f298d23c60c68b05edb478b30
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-07-11 10:13:49 -0400

    Add files via upload

--- main.cc
+++ main.cc
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
                            ::toupper);
             if (!strcmp(argv[1], "--HELP")) {
                 print_help();
-                return 0;
+                return EXIT_SUCCESS;
             }
         }
         case 3:
