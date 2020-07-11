cc6684fd75864bb6cd45404c635969603ef4c9e6
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-07-11 13:31:06 -0400

    Add files via upload

--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@ static auto print_help() -> void {
     std::cout << "use swap --help to print this message\n";
 }
 
-static auto validate_files(const std::filesystem::path &file1_path, const std::filesystem::path &file2_path) -> void {
+static auto validate_files(const std::filesystem::path& file1_path, const std::filesystem::path& file2_path) -> void {
     {
         /* check if file exists */
         const auto file1_exists = std::filesystem::exists(file1_path);
@@ -18,8 +18,8 @@ static auto validate_files(const std::filesystem::path &file1_path, const std::f
 
 
         if (!exists) {
-            if (!file1_exists) std::cerr << "cannot find file " << file1_path.c_str() << '\n';
-            if (!file2_exists) std::cerr << "cannot find file " << file2_path.c_str() << '\n';
+            if (!file1_exists) std::cerr << "cannot find file " << file1_path << '\n';
+            if (!file2_exists) std::cerr << "cannot find file " << file2_path << '\n';
             exit(EXIT_FAILURE);
         }
     }
@@ -32,34 +32,39 @@ static auto validate_files(const std::filesystem::path &file1_path, const std::f
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     std::ios::sync_with_stdio(false);
     switch (argc) {
-        case 2: {
-            /* convert the second arg to upper case */
-            std::transform(argv[1],
-                           argv[1] + strlen(argv[1]),
-                           argv[1],
-                           ::toupper);
-            if (!strcmp(argv[1], "--HELP")) {
-                print_help();
-                return EXIT_SUCCESS;
-            }
+    case 2: {
+        /* convert the second arg to upper case */
+        std::transform(argv[1],
+            argv[1] + strlen(argv[1]),
+            argv[1],
+            ::toupper);
+        if (!strcmp(argv[1], "--HELP")) {
+            print_help();
+            return EXIT_SUCCESS;
         }
-        case 3:
-            break;
-        default: {
+        else {
             std::cerr << "Invalid args see --help for usage\n";
-            return EXIT_FAILURE;
+            return EXIT_SUCCESS;
         }
     }
-    const auto &file1_path = std::filesystem::path{argv[1]};
-    const auto &file2_path = std::filesystem::path{argv[2]};
+    case 3:
+        break;
+    default: {
+        std::cerr << "Invalid args see --help for usage\n";
+        return EXIT_FAILURE;
+    }
+    }
+    const auto& file1_path = std::filesystem::path{ argv[1] };
+    const auto& file2_path = std::filesystem::path{ argv[2] };
 
 
     validate_files(file1_path, file2_path);
     char temp_filename[L_tmpnam];
-    const auto &temp_filepath = std::filesystem::current_path().append(std::tmpnam(temp_filename));
+    tmpnam_s(temp_filename, L_tmpnam);
+    const auto& temp_filepath = std::filesystem::path{ temp_filename };
     std::filesystem::rename(file1_path, temp_filepath);
     std::filesystem::rename(file2_path, file1_path);
     std::filesystem::rename(temp_filepath, file2_path);
